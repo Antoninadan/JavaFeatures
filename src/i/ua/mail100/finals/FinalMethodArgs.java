10a91149d5e73dc16ed26b0650b53c88cae3b481
Antoninadan <mail100@i.ua>
2020-06-22 18:57:26 +0300

    add test final method args

---
+++ src/i/ua/mail100/finals/FinalMethodArgs.java
@@ -0,0 +1,36 @@
+package i.ua.mail100.finals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FinalMethodArgs {
+
+    public static void main(String args[]) {
+        FinalMethodArgs obj = new FinalMethodArgs();
+        obj.myMethod(10, "BeginnersBook.com");
+    }
+
+    public void myMethod(int num, final String str) {
+        // This is allowed as num is not final
+        num = num + 10;
+
+        /* This is not allowed as String str is final and
+         * we cannot change the value of final parameter.
+         * we can just use it without modifying its value.
+         */
+//        str = str+"XYZ";
+
+        System.out.println(num + str);
+    }
+
+    public void myMethodTwo(int num, final List<String> strings) {
+        // This is allowed as num is not final
+        num = num + 10;
+
+        // not allowed
+//        strings = new ArrayList<>();
+
+        System.out.println(num + str);
+    }
+
+}

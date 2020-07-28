14f0ceb9a3e4df62b349866755506c8feca40f60
daniloap <mail100@i.ua>
2020-07-28 15:45:05 +0300

    test commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import i.ua.mail100.reflection.ReflectionExample;
 public class Main {
     private static final String f = "12313";
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World !");
 
         System.out.println(ReflectionExample.getModifiers(f));
     }
